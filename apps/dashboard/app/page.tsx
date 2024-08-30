52f2a8bc609467b509b22c75912c61a9b4397202
eraykeskinmac <eraykeskinmac@gmail.com>
2024-08-30 18:38:57 +0300

    turborepo shadcn/ui tailwindcss initial

---
+++ apps/dashboard/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from "@ui/components/button";
+
+export default function Page(): JSX.Element {
+  return (
+    <div>
+      <Button variant="outline">Observer</Button>
+    </div>
+  );
+}
