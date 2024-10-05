08b9ac82e1bed70865fd2b103a76f15ff5a67139
eraykeskinmac <eraykeskinmac@gmail.com>
2024-10-05 23:25:03 +0300

    Refactor ClickHouse client configuration

--- apps/dashboard/app/api/client.ts
+++ apps/dashboard/app/api/client.ts
@@ -1,10 +1,13 @@
 import { createClient } from "@clickhouse/client";
 
 const client = createClient({
-  url: process.env.CLICKHOUSE_HOST,
+  host: process.env.CLICKHOUSE_HOST,
   username: process.env.CLICKHOUSE_USERNAME || '',
   password: process.env.CLICKHOUSE_PASSWORD || '',
   database: process.env.CLICKHOUSE_DATABASE || '',
+  tls: {
+    rejectUnauthorized: false // Not recommended for production
+  } as any
 });
 
 export default client;
